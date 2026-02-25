03020491c2abd0804ffba0d6d78718e862212ba9
Diego Ricardo <diegoricardoweb@gmail.com>
2026-02-25 00:06:32 -0300

    feat: add get participant

---
+++ back/src/routes/get-participant.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { ClientError } from "../errors/client-error";
+import { prisma } from "../lib/prisma";
+
+export async function getParticipant(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get('/participants/:participantId', {
+    schema: {
+      params: z.object({
+        participantId: z.uuid()
+      })
+    }
+  } , async (request) => {
+    const { participantId } = request.params
+
+    const participant = await prisma.participant.findUnique({
+      where: {
+        id: participantId
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        isConfirmed: true
+      }
+    })
+
+    if (!participant) {
+      throw new ClientError("Participant not found")
+    }
+
+    return { participant }
+  })
+}
